51bdeca58950e9963ee07b4a59dd042885814f18
Tomas-Pierce-at-UNC <89414673+Tomas-Pierce-at-UNC@users.noreply.github.com>
2023-06-13 11:37:13 -0400

    try method with morpology and neural net

--- proboscis4.py
+++ proboscis4.py
@@ -14,7 +14,7 @@ import numpy as np
 
 from cine import Cine
 import tube2
-from skimage import measure
+from skimage import measure, morphology
 # import align
 
 import glob
@@ -54,73 +54,101 @@ import os
 # def get_difference(frame, med):
 #     return frame.astype(np.float32) - med.astype(np.float32)
 
-if not os.path.isdir("./proboscis_canidates"):
-    os.mkdir("./proboscis_canidates")
+def main():
+    if not os.path.isdir("./proboscis_canidates"):
+        os.mkdir("./proboscis_canidates")
+    
+    model = keras.models.load_model("proboscis_utils/proboscis_model_b1")
+    names = glob.glob("data2/**/*.cine", recursive=True)
+    names = reversed(names)
+    #data = {}
+    for name in names:
+        measurements = []
+        cine = Cine(name)
+        # med = cine.get_video_median()
+        # med = tube2.tube_crop1(med)
+        for i in range(0,cine.image_count, 16):
+            if i + 16 >= cine.image_count:
+                break
+            images = []
+            for j in range(i,i+16):
+                frame = cine.get_ith_image(j)
+                tube = tube2.tube_crop1(frame)
+                images.append(tube)
+            try:
+                in_arr = np.array(images)
+            except ValueError as e:
+                print(e)
+                continue
+            in_arr = in_arr[...,np.newaxis]
+            if in_arr.shape != (16, 600, 100, 1):
+                print(in_arr.shape)
+                print("shape is wrong")
+                continue
+            out = model(in_arr)
+            out_arr = out.numpy()
+            for k in range(0,16):
+                prediction = out_arr[k,:,:,0]
+                #p = np.empty_like(prediction)
+                #p[:] = prediction
+                #p[p < 0] = 0
+                #p = p / p.max()
+                mask = prediction > 0
+                ero = morphology.erosion(mask)
+                labels = measure.label(ero)
+                regions = measure.regionprops(labels, images[k])
+                regions = [region for region in regions if region.area > 100]
+                idx = i + k
+                for region in regions:
+                    bbox = region.bbox
+                    label = region.label
+                    area = region.area
+                    feret = region.feret_diameter_max
+                    orient = region.orientation
+                    major_axis_len = region.axis_major_length
+                    minor_axis_len = region.axis_minor_length
+                    centroid = region.centroid
+                    w_centroid = region.centroid_weighted
+                    eq_diam = region.equivalent_diameter_area
+                    eu = region.euler_number
+                    i_max = region.intensity_max
+                    i_mean = region.intensity_mean
+                    i_min = region.intensity_min
+                    perim = region.perimeter
+                    perim_c = region.perimeter_crofton
+                    solid = region.solidity
+                    datum = (idx,
+                             *bbox,
+                             label,
+                             area,
+                             feret,
+                             orient,
+                             major_axis_len,
+                             minor_axis_len,
+                             *centroid,
+                             *w_centroid,
+                             eq_diam,
+                             eu,
+                             i_max,
+                             i_mean,
+                             i_min,
+                             perim,
+                             perim_c,
+                             solid,
+                             region.num_pixels,
+                             region.area_bbox,
+                             region.area_convex,
+                             region.area_filled,
+                             region.eccentricity,
+                             region.extent,
+                             )
+                    measurements.append(datum)
+                #imshow(p > 0.1)
+                #pyplot.show()
+        mmts = np.array(measurements)
+        np.savetxt("./proboscis_canidates/{}.tsv".format(pathlib.Path(name).stem), mmts, delimiter='\t')
+        cine.close()
 
-model = keras.models.load_model("proboscis_utils/proboscis_model_b1")
-names = glob.glob("data2/**/*.cine", recursive=True)
-names = reversed(names)
-#data = {}
-for name in names:
-    measurements = []
-    cine = Cine(name)
-    # med = cine.get_video_median()
-    # med = tube2.tube_crop1(med)
-    for i in range(0,cine.image_count, 16):
-        if i + 16 >= cine.image_count:
-            break
-        images = []
-        for j in range(i,i+16):
-            frame = cine.get_ith_image(j)
-            tube = tube2.tube_crop1(frame)
-            images.append(tube)
-        try:
-            in_arr = np.array(images)
-        except ValueError as e:
-            print(e)
-            continue
-        in_arr = in_arr[...,np.newaxis]
-        if in_arr.shape != (16, 600, 100, 1):
-            print(in_arr.shape)
-            print("shape is wrong")
-            continue
-        out = model(in_arr)
-        out_arr = out.numpy()
-        for k in range(0,16):
-            prediction = out_arr[k,:,:,0]
-            p = np.empty_like(prediction)
-            p[:] = prediction
-            p[p < 0] = 0
-            p = p / p.max()
-            mask = p > 0.1
-            labels = measure.label(mask)
-            regions = measure.regionprops(labels, images[k])
-            idx = i + k
-            for region in regions:
-                bbox = region.bbox
-                label = region.label
-                area = region.area
-                feret = region.feret_diameter_max
-                orient = region.orientation
-                major_axis_len = region.axis_major_length
-                minor_axis_len = region.axis_minor_length
-                centroid = region.centroid
-                w_centroid = region.centroid_weighted
-                eq_diam = region.equivalent_diameter_area
-                eu = region.euler_number
-                i_max = region.intensity_max
-                i_mean = region.intensity_mean
-                i_min = region.intensity_min
-                perim = region.perimeter
-                perim_c = region.perimeter_crofton
-                solid = region.solidity
-                datum = (idx, *bbox, label, area, feret, orient,
-                         major_axis_len, minor_axis_len, *centroid,
-                         *w_centroid, eq_diam, eu, i_max, i_mean, i_min,
-                         perim, perim_c, solid)
-                measurements.append(datum)
-            #imshow(p > 0.1)
-            #pyplot.show()
-    mmts = np.array(measurements)
-    np.savetxt("./proboscis_canidates/{}.tsv".format(pathlib.Path(name).stem), mmts, delimiter='\t')
-    cine.close()
+
+if __name__ == '__main__':
+    main()
