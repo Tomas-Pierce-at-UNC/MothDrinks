c1a40deee27087be272475af265e8f3ea0ca3ace
Tomas-Pierce-at-UNC <89414673+Tomas-Pierce-at-UNC@users.noreply.github.com>
2023-06-15 16:59:30 -0400

    finish proboscis tracking

--- proboscis4.py
+++ proboscis4.py
@@ -21,7 +21,7 @@ import glob
 import json
 import pathlib
 import os
-# import random
+import random
 
 # model = keras.models.load_model("proboscis_utils/proboscis_model_b1")
 
@@ -53,6 +53,39 @@ import os
 
 # def get_difference(frame, med):
 #     return frame.astype(np.float32) - med.astype(np.float32)
+
+def difference(left, right):
+    return left.astype(np.int32) - right.astype(np.int32)
+
+def batch_difference(batch, right):
+    out = np.empty_like(batch, dtype=np.int32)
+    for i in range(len(batch)):
+        out[i,:,:,0] = difference(batch[i,:,:,0], right)
+    return out
+
+def load_batch(video, i, batch_size=16):
+    frames = []
+    for j in range(i, i+batch_size):
+        frame = video.get_ith_image(j)
+        tube = tube2.tube_crop1(frame)
+        frames.append(tube)
+    try:
+        batch = np.array(frames)
+    except ValueError as e:
+        print(e)
+        return None
+    return batch[..., np.newaxis]
+
+def predict_batch(batch, p_model, thresh=0.1):
+    p = p_model(batch)
+    pred = p.numpy()
+    masks = pred >= thresh
+    out = np.empty_like(masks)
+    for i in range(len(batch)):
+        out[i,:,:,0] = morphology.remove_small_objects(masks[i,:,:,0],
+                                                       min_size=50,
+                                                       connectivity=2)
+    return out
 
 def main():
     if not os.path.isdir("./proboscis_canidates"):
@@ -93,61 +126,83 @@ def main():
                 #p[:] = prediction
                 #p[p < 0] = 0
                 #p = p / p.max()
-                mask = prediction > 0
-                ero = morphology.erosion(mask)
-                labels = measure.label(ero)
-                regions = measure.regionprops(labels, images[k])
-                regions = [region for region in regions if region.area > 100]
-                idx = i + k
-                for region in regions:
-                    bbox = region.bbox
-                    label = region.label
-                    area = region.area
-                    feret = region.feret_diameter_max
-                    orient = region.orientation
-                    major_axis_len = region.axis_major_length
-                    minor_axis_len = region.axis_minor_length
-                    centroid = region.centroid
-                    w_centroid = region.centroid_weighted
-                    eq_diam = region.equivalent_diameter_area
-                    eu = region.euler_number
-                    i_max = region.intensity_max
-                    i_mean = region.intensity_mean
-                    i_min = region.intensity_min
-                    perim = region.perimeter
-                    perim_c = region.perimeter_crofton
-                    solid = region.solidity
-                    datum = (idx,
-                             *bbox,
-                             label,
-                             area,
-                             feret,
-                             orient,
-                             major_axis_len,
-                             minor_axis_len,
-                             *centroid,
-                             *w_centroid,
-                             eq_diam,
-                             eu,
-                             i_max,
-                             i_mean,
-                             i_min,
-                             perim,
-                             perim_c,
-                             solid,
-                             region.num_pixels,
-                             region.area_bbox,
-                             region.area_convex,
-                             region.area_filled,
-                             region.eccentricity,
-                             region.extent,
-                             )
-                    measurements.append(datum)
+                mask = prediction > 0.1
+                big = morphology.remove_small_objects(mask, min_size=50, connectivity=2)
+                label = measure.label(big)
+                regiontable = measure.regionprops_table(label,properties=('label','bbox','eccentricity'))
+                datum = max(regiontable['bbox-2'], default=-10)
+                measurements.append((i+k, datum))
                 #imshow(p > 0.1)
                 #pyplot.show()
         mmts = np.array(measurements)
         np.savetxt("./proboscis_canidates/{}.tsv".format(pathlib.Path(name).stem), mmts, delimiter='\t')
         cine.close()
+
+
+def measure_proboscis_position(videoname):
+    #main()
+    prob_model = keras.models.load_model('proboscis_utils/proboscis_model_b1')
+    video = Cine(videoname)
+    m = video.get_video_median()
+    median_img = tube2.tube_crop1(m)
+    measurements = []
+    for i in range(0, video.image_count, 16):
+        print("*")
+        if i + 15 >= video.image_count:
+            break
+        batch = load_batch(video, i)
+        if batch is None:
+            continue
+        if batch.shape != (16, 600, 100, 1):
+            continue
+        difs = batch_difference(batch, median_img)
+        p = predict_batch(batch, prob_model)
+        for j in range(16):
+            lbl = measure.label(p[j,:,:,0])
+            regions = measure.regionprops(lbl, difs[j,:,:,0])
+            regions.sort(key = lambda region : region.bbox[2], reverse=True)
+            for region in regions:
+                if region.intensity_mean < 0:
+                    measurements.append(((i+j), region.bbox[2]))
+                    break
+                    
+    video.close()
+    return measurements
+
+def display(lookup, vidname):
+    video = Cine(vidname)
+    for i in range(video.image_count):
+        if i in lookup:
+            y = lookup[i]
+            frame = video.get_ith_image(i)
+            imshow(frame)
+            pyplot.hlines(y, 50, 750, colors='red', linestyle='dashed')
+            pyplot.show(block=False)
+            pyplot.pause(0.25)
+            pyplot.close()
+    video.close()
+
+
+def main():
+    if not os.path.isdir("./proboscis_measurements"):
+        os.mkdir("./proboscis_measurements")
+    vids = glob.glob("data2/*.cine")
+    vids.extend(glob.glob("data2/unsuitableVideos/*.cine"))
+    vids.reverse()
+    for name in vids:
+        try:
+            measurements = measure_proboscis_position(name)
+        except ValueError as e:
+            print(e)
+            print(name)
+            continue
+        mmts = np.array(measurements)
+        stem = pathlib.Path(name).stem
+        np.savetxt("./proboscis_measurements/{}.tsv".format(stem),
+                   mmts,
+                   delimiter='\t'
+                   )
+        print('#')
 
 
 if __name__ == '__main__':
